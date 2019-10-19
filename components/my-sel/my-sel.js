02b21afb9d18b8c106fcf1e8a49ae21bce96cf95
chengyi3192 <244461001@qq.com>
2019-10-19 22:47:24 +0800

    组件化开发

---
+++ components/my-sel/my-sel.js
@@ -0,0 +1,17 @@
+Component({
+  properties: {
+    
+  },
+
+  data: {
+    counter: 0
+  },
+
+  methods: {
+    incrementCounter(num){
+      this.setData({
+        counter:this.data.counter + num
+      })
+    }
+  }
+})
